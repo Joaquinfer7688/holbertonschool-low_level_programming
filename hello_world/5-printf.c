fbaffa200bd1000b46e20019ff6909567c0cfaaa
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-10 10:55:32 -0700

    lenguajec

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - 
+ * main - print the string 
  *
  *return: 0
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art\")
+	printf("with proper grammar, but the outcome is a piece of art\")
 		return:(0);
 }
