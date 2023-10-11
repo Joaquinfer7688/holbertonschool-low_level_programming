fd7de9fc3b5e98c2dd3b1a97a3949036d082ca68
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-11 10:32:22 -0700

    lenguajec

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - single-digit numbers
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int n, i;
+
+	for (n = 0; n <= 9; n++)
+	{
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d%d ", n, i);
+}
+}
+
+printf("\n");
+return (0);
+}
