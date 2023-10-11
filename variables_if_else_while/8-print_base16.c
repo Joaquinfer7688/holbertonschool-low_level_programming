246cbaf976abf9bed60fa251ff041c26bc8306e1
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-11 10:08:07 -0700

    lenguajec

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Hexadecimal
+ *
+ * return:0
+ */
+int main(void)
+{
+	char hexDigit;
+
+	for (hexDigit = '0'; hexDigit <= '9'; hexDigit++)
+
+	putchar('\n');
+	return (0);
+}
