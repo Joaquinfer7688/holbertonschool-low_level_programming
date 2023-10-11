b624552861c60abb7ec2b24744955ccef2301100
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-11 10:46:27 -0700

    lenguajec

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; c++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 
-		if 8c != '9')
+		if (n != '9')
 		{
 			putchar(',');
-			putchar('');
+			putchar(' ');
 		}
 	}
 	printf("\n");
