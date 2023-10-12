574829837ddde3f39688184b4383a64fb611c5d7
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-12 06:12:20 -0700

    lenguajec

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - the alphabet game
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	
+		putchar(letter);
+
+	putchar('\n');
+		return (0);
+}
+
