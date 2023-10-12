13020b21f9ae90ef30f76d95a4003fae582f14cb
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-12 10:33:51 -0700

    lenguajec

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,22 +1,18 @@
 #include "main.h"
+
 /**
  * _isalpha - Checks for alphabetic character
- *@c: the character to be checked
+ *@c: The character to be checked
  *
- * Return:1 for lowercase and uppercase or 0 for anything else
+ * Return: 1 for lowercase and uppercase or 0 for anything else
  */
+
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
-	{
-		else if (c >= 65 && c <= 90)
-
-			return (o);
-	}
-
-		return (0);
+			return (0);
 }
 
