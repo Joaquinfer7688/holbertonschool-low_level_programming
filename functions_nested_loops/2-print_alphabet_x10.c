6f00dbb68fb17c8a4d9f830c5eefbf58d1836dd5
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-12 06:57:21 -0700

    lenguajec

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,18 @@
 void print_alphabet_x10(void)
 {
 	char letter;
+	int i = 0; 
 
-	for (int i = 0; i < 10; i++)
+	while (i <= 9)
+	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 
 	{
 		_putchar(letter);
 	}
+
 	_putchar('\n');
+	
+	i++;
+	}
 }
