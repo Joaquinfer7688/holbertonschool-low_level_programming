40dad5d5e8adec93de3f476121507fd722d33a6f
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-13 09:59:19 -0700

    lenguajec

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - write function that checks for uppercase character
- *@c: hte number to be checked
- * Return: 1 if c is uppercase or 0 for anything else
- */
+* _isupper - write function that checks for uppercase character
+*@c: hte number to be checked
+* Return: 1 if c is uppercase or 0 for anything else
+*/
 int _isupper(int c)
 {
-	if ('c' >= 65 && 'c' <= 90)
+	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
