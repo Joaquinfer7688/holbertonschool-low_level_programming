4cf9ffb1c71bde5cd7932bb15b6a8586c5d9b204
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-13 09:52:11 -0700

    lenguajec

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - write function that checks for uppercase character
+ *
+ * Return: 1 if c is uppercase or 0 for anything else
+ */
+int _issuper(int c);
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+		return (0);
+}
