aec9442f324eb978743162f40330382e71c1c7ba
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-13 10:15:41 -0700

    lenguajec

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function than multiplies two integers
+ *@a: the first number
+ *@b: the second number
+ *return:0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
