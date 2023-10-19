a00069d3ad18c5f3659fd6ef2a38cd8620c4ca2a
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-19 06:52:39 -0700

    punteros

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers
- *
- *
+ *@a: array to be printed
+ *@n: number of elements to print
  */
 void print_array(int *a, int n)
 {
