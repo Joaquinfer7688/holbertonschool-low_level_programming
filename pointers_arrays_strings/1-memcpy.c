67f7f3e570c74144c8cc638b302b2a775f161a61
Joaquinfer7688 <7688@holbertonschool.com>
2023-10-23 07:45:15 -0700

    punteros

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _mempcy - function that copies memory area
+ * _memcpy - function that copies memory area
  *@dest: dest memory area
  *@src: source memory area
  *@n: bytes from memory area to copy
  *Return: a pointer to dest
  */
-char *_mempcy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
