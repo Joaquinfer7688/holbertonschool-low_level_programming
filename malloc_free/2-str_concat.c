a756a0df384eb2abbc0cbc9438da64b90d83f86b
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-01 09:38:38 -0700

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,9 +3,9 @@
 #include "main.h"
 /**
  * str_concat - concatenates two strings
- *@s1: first string to concatenate
- *@s2: second string to concatenate
- *Return: pointer to new string created or NULL
+ *@s1: string to concatenate
+ *@s2: other string to concatenate
+ *Return: pointer to new string created (success) or NULL (error)
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2 && s2[b])
 		b++;
 
-	s3 = malloc(sizeof(char) * (a + b + a));
+	s3 = malloc(sizeof(char) * (a + b + 1));
 	if (s3 == NULL)
 		return (NULL);
 
@@ -29,9 +29,20 @@ char *str_concat(char *s1, char *s2)
 		while (i < a)
 		{
 			s3[i] = s1[i];
+			i++;
+		}
+	}
+	if (s2)
+	{
+		while (i < (a + b))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
 		}
 	}
 	s3[i] = '\0';
+
 	return (s3);
 }
 
