67b4812d667f5e31ebcf8ece47eeef37ffaca996
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-06 05:31:26 -0800

    structures

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - name dog
+ *@name: name the dog
+ *@age: age the dog
+ *@owner: owner the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
