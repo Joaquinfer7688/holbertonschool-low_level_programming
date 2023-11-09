bcff77f8f92fba4c69764d46dbb5fe2ed7ce3624
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-09 12:19:15 -0800

    pointerafunction

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that search an integer
+ *@size: number of elements in array
+ *@cmp: pointer used to compare values
+ *@array: ...
+ */
+int int_index(int *array, int size, int (*cmp) (int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
