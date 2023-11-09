759956e880234d1b32996945fbe10f1fa05c37a5
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-09 11:51:00 -0800

    pointerafunction

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
 /**
  * array_iterator - function that execute a function
- *@array: ...
- *@size: of the array
+ *@array: array to go
+ *@size: size the array
  *@action: pointer to the function
  */
 void array_iterator(int *array, size_t size, void (*action) (int))
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	if (array != NULL && action != NULL && size > 0)
 	{
