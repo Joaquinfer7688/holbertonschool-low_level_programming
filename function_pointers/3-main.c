5da48f04c975931537325dbc0ffa0eaa01ebdc28
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-09 14:29:26 -0800

    pointerafunction

---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - ..
+  * @argc: ..
+  * @argv: ..
+  * Return: ..
+  */
+int main(int argc, char *argv[])
+{
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operator = get_op_func(argv[2]);
+
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
