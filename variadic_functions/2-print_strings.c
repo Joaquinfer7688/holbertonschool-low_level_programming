5f11902124682428047aa77cd933e8a12b915012
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-10 11:33:28 -0800

    variadicfunctions

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *);
+		j = va_arg(args, char *);
 
 		if (separator != NULL && i > 0)
 			printf("%s", separator);
