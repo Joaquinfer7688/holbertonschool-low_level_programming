3815d0ce2022c88a77513f7c3e0c15315f2b16d9
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2023-11-23 11:06:03 -0300

    task 4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 /**
-*dlistint- function that free list
+*free_dlistint- function that free list
 *@head: pointer to list
-*Return: 0
 */
 void free_dlistint(dlistint_t *head)
 {
