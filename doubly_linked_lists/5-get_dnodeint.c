65f578076769d8a0124132f3d25940bfdcd95af0
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2023-11-24 11:59:35 -0300

    task5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "lists.h"
 /**
  *get_dnodeint_at_index -
@@ -17,22 +16,3 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	return (head);
 }
-=======
-#include "lists.h"
-/*
-*get_dnodeint_at_index - return the nth node of the list
-*@head: pointer to list
-*@index: index with node
-*Return: NULL or head
-*/
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-    for (; index != 0; index--)
-    {
-        if (head == NULL)
-        return (NULL);
-        head = head->next;
-    }
-    return (head);
-}
->>>>>>> ff3ce73ea760f896b378d5f7ef661a42a5f39b85
