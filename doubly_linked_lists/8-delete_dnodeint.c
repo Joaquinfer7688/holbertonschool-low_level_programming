6996703f8002ab58c39bbef73f32f3219c021347
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-28 13:05:42 -0800

    doublylinkedlists

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ *delete_dnodeint_at_index: function deletes node an index
+ *@head: pointer
+ *@index: index position node delete
+ *Return: 1 or -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *j = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (j == NULL)
+			return (-1);
+		j = j->next;
+	}
+
+	if (j == *head)
+	{
+		*head = j->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		j->prev->next = j->next;
+		if (j->next != NULL)
+			j->next->prev = j->prev;
+	}
+
+		free(j);
+		return (1);
+}
