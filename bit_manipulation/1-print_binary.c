0a32e06b377db26153f8051ef7cb0729e24e90bf
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-27 16:13:38 -0800

    bitmanipulation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,4 +6,18 @@
  */
 void print_binary(unsigned long int n)
 {
+	int temp;
+	static int cont;
 
+	if (n == 0 && cont > 0)
+		return;
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	temp = (n & 1);
+	cont++;
+	print_binary(n >>= 1);
+	_putchar('0' + temp);
+}
