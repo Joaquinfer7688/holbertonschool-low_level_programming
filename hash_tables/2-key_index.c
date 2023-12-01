27289a485866e226f0ed24a7d6e96e101b074e6e
Joaquinfer7688 <7688@holbertonschool.com>
2023-12-01 09:12:50 -0800

    hashtables

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ *key_index - function that ghives you index a key
+ *@size: size of array
+ *@key: key get index
+ *Return: index of the key 
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
