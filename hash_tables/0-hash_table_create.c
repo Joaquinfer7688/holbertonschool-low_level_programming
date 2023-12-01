66bdb5ababb123bd01c62aebab99662096d5e4ca
Joaquinfer7688 <7688@holbertonschool.com>
2023-12-01 07:21:49 -0800

    hashtables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 /**
  *hash_table_create - function that create a hash table
  *@size: size the array
- *Retun: NULL or hash_table
+ *Retun: NULL or hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
