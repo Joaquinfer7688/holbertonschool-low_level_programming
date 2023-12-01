92ee8539c165e5f294e98aab9984def1ee922615
Joaquinfer7688 <7688@holbertonschool.com>
2023-12-01 09:13:36 -0800

    hashtables

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,7 +3,7 @@
  *key_index - function that ghives you index a key
  *@size: size of array
  *@key: key get index
- *Return: index of the key 
+ *Return: index of the key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
