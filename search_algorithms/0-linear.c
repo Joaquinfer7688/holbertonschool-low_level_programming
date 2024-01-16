e9654819de11ebe75706f1422f08851160a06557
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-16 15:02:08 -0300

    task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -8,12 +8,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+    size_t i;
+
     if (array == NULL)
     {
         return (-1);
     }
 
-    for (size_t i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", array[i], value);
 
